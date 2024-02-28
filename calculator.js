56689dc968b8466b5a46749c4c41582225816c4d
ragunath-br <153410999+ragunath-br@users.noreply.github.com>
2024-02-28 13:18:02 +0530

    CREATES FN TO CALL OTHER FN BASED ON OPERATOR
    Operate function will call on the different function based on the value of operator.

--- calculator.js
+++ calculator.js
@@ -22,3 +22,17 @@ function divide(a,b){
     return a/b;
 }
 
+//Function to call the above functions based on the operator
+
+function operate(firstNumber,secondNumber,operator){
+    switch(operator){
+        case '+':
+            return add(firstNumber,secondNumber);
+        case '-':
+            return subtract(firstNumber,secondNumber);  
+        case '*':
+            return multiply(firstNumber,secondNumber);
+        case '/':
+            return divide(firstNumber,secondNumber);  
+    }
+}
