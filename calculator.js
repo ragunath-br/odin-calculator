acda03509aaf5a768aada0f408c25aac53e151b9
ragunath-br <153410999+ragunath-br@users.noreply.github.com>
2024-02-28 13:04:09 +0530

    CREATES FUNCTIONS FOR BASIC OPERATIONS
    All the functions created will take two inputs perform an operation and return a value.
    +,-,*,/ are the operations performed.

---
+++ calculator.js
@@ -0,0 +1,17 @@
+//Functions to perform the basic mathematical operations with two operands
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
